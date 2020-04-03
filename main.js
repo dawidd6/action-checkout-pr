bc7b96f18ce9ce4b549770adf47ef3894c810af5
Dawid Dziurla <dawidd0811@gmail.com>
2020-04-03 09:47:40 +0200

    main: print pull

--- main.js
+++ main.js
@@ -16,6 +16,8 @@ async function main() {
             pull_number: pr
         })
 
+        console.log(pull)
+
         const fork = pull.data.head.repo.fork
         const modify = pull.data.maintainer_can_modify
         const remote = fork ? pull.data.head.repo.clone_url : "origin"
